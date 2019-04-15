c6481dbd176bc33fea9e099796503db84f29447d
Angel Chou <cat77120377@hotmail.com>
2019-04-15 17:49:43 +0800

    Initial Commit

---
+++ HomeworkListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeworkListApp
+//
+//  Created by Shun-Ching, Chou on 2019/4/15.
+//  Copyright © 2019 Shun-Ching, Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
